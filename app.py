05d2680cbb44d355c4718e35f914440703fac51d
AbiOppenheim <oppenabi@gmail.com>
2023-04-09 13:36:08 -0300

    funciona bien - falta diseño

--- app.py
+++ app.py
@@ -18,18 +18,32 @@ def index():
             second_instrument = request.form['second_instrument{}'.format(i)]
             people_data.append((name, first_instrument, second_instrument))
         result = process_data(people_data)
-        return redirect(url_for('result', results=str(result)))
+        return redirect(url_for('result', results=str(result), currentResultIndex=0, numResults=len(result)))
     else:
         return render_template('index.html')
 
-@app.route('/result')
+@app.route('/result', methods=['GET', 'POST'])
 def result():
     results_str = request.args.get('results')
+    print('all the results:', results_str)
+    currentResultIndex = int(request.args.get('currentResultIndex', '0'))
+    numResults = int(request.args.get('numResults', '0'))
     if results_str:
         results = ast.literal_eval(results_str)
     else:
         results = []
-    return render_template('result.html', results=results)
+    currentResult = []
+    if currentResultIndex < len(results):
+        currentResult = results[currentResultIndex]
+    print('currentResult:', currentResult)
+    print('currentResultIndex:', currentResultIndex)
+    if request.method == 'POST':
+        if 'currentResultIndex' in request.form:
+            currentResultIndex = int(request.form['currentResultIndex'])
+            results_str = request.form['results']
+        return redirect(url_for('result', results=results_str, currentResultIndex=currentResultIndex, numResults=len(results)))
+    return render_template('result.html', results=results, currentResultIndex=currentResultIndex, numResults=numResults, currentResult=currentResult, results_str=results_str)
+
 
 def process_data(people_data):
     # Do some processing on the data
@@ -53,7 +67,7 @@ def process_data(people_data):
         for i, option in enumerate(bits[:3]):
             result = []
             for name, bit in zip(participants, option[1]):
-                print(f'{name} -> {participants[name][bit]}')
+                # print(f'{name} -> {participants[name][bit]}')
                 result.append('{} -> {}'.format(name, participants[name][bit]))
             results.append(result)
     else:
@@ -74,11 +88,12 @@ def process_data(people_data):
             for instrument, names in ins.items():
                 selected_participants.add(names[0])
                 selected_instruments.add(instrument)
-                print(f'{names[0]} -> {instrument}')
+                # print(f'{names[0]} -> {instrument}')
                 result.append('{} -> {}'.format(names[0], instrument))
             result.append('Con los instrumentos que sobran: {}'.format(instruments-selected_instruments))
             result.append('Y personas sin asignar: {}'.format(participants.keys()-selected_participants))
             results.append(result)
     return results
+
 if __name__ == '__main__':
     app.run(debug=True)
